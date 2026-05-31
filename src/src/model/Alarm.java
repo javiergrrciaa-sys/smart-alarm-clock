bff7ba8ca30adcaebb39d616212bab5e805361cb
javiergrrciaa-sys <javiergrrciaa@gmail.com>
2026-05-31 13:35:54 +0200

    Implement weekly repeat feature in Alarm class
    Added functionality for weekly repetition of alarms.

--- src/src/model/Alarm.java
+++ src/src/model/Alarm.java
@@ -1,6 +1,9 @@
 package model;
 
 import java.time.LocalTime;
+import java.time.DayOfWeek;
+import java.util.Set;
+import java.util.HashSet;
 import java.util.UUID;
 
 public class Alarm {
@@ -10,13 +13,20 @@ public class Alarm {
     private LocalTime time;
     private boolean active;
 
+    // Repetición semanal
+    private Set<DayOfWeek> repeatDays;
+
     public Alarm(String label, LocalTime time) {
         this.id = UUID.randomUUID();
         this.label = label;
         this.time = time;
         this.active = true;
+
+        // Inicialización de repetición semanal
+        this.repeatDays = new HashSet<>();
     }
 
+    // GETTERS
     public UUID getId() {
         return id;
     }
@@ -33,6 +43,11 @@ public class Alarm {
         return active;
     }
 
+    public Set<DayOfWeek> getRepeatDays() {
+        return repeatDays;
+    }
+
+    // SETTERS
     public void setLabel(String label) {
         this.label = label;
     }
@@ -41,14 +56,29 @@ public class Alarm {
         this.time = time;
     }
 
+    // ACTIVAR / DESACTIVAR
     public void activate() {
-        active = true;
+        this.active = true;
     }
 
     public void deactivate() {
-        active = false;
+        this.active = false;
     }
 
+    // REPETICIÓN SEMANAL
+    public void addRepeatDay(DayOfWeek day) {
+        repeatDays.add(day);
+    }
+
+    public void removeRepeatDay(DayOfWeek day) {
+        repeatDays.remove(day);
+    }
+
+    public boolean repeatsOn(DayOfWeek day) {
+        return repeatDays.contains(day);
+    }
+
+    // TO STRING
     @Override
     public String toString() {
         return "Alarm{" +
@@ -56,6 +86,7 @@ public class Alarm {
                 ", label='" + label + '\'' +
                 ", time=" + time +
                 ", active=" + active +
+                ", repeatDays=" + repeatDays +
                 '}';
     }
 }
